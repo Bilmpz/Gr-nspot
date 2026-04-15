328a43a578a3260f573a45cf3597947c6b042b0e
Oliver Løbel <oliver.lobel@yahoo.dk>
2026-04-15 19:10:15 +0200

    Title

--- backend/CheapHours.Api/obj/Debug/net10.0/CheapHours.Api.AssemblyInfo.cs
+++ backend/CheapHours.Api/obj/Debug/net10.0/CheapHours.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CheapHours.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b0d6d8aa80ec4266c236fbf149ce20b522546c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CheapHours.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CheapHours.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
